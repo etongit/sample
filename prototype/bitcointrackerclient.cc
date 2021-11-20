c4dd37604bc511002cf7f31e6b449989445995d7
Aakanksha Singh <aakanksha.cingh@gmail.com>
2021-11-20 10:07:31 -0800

    add the transfer detect functionality

--- prototype/bitcointrackerclient.cc
+++ prototype/bitcointrackerclient.cc
@@ -17,6 +17,8 @@ using grpc::Status;
 using helloworld::BitcoinTracker;
 using helloworld::AddAddressRequest;
 using helloworld::AddAddressResponse;
+using helloworld::DetectTransactionsResponse;
+using helloworld::DetectTransactionsRequest;
 using std::string;
 
 class BitcoinTrackerClient {
@@ -27,7 +29,7 @@ class BitcoinTrackerClient {
   // Assembles the client's payload, sends it and presents the response back
   // from the server.
   std::string AddAddress(const std::string& user, const std::string& address) {
-    // Add the new address to be sent to the servers.
+    // Data we are sending to the server.
     AddAddressRequest request;
     request.set_username(user);
     *request.add_address() = address;
@@ -35,7 +37,7 @@ class BitcoinTrackerClient {
     AddAddressResponse reply;
     ClientContext context;
 
-    // Make the RPC!
+    // The actual RPC.
     Status status = stub_->AddAddress(&context, request, &reply);
 
     // Act upon its status.
@@ -48,6 +50,32 @@ class BitcoinTrackerClient {
       std::cout << status.error_code() << ": " << status.error_message()
                 << std::endl;
       return "RPC failed";
+    }
+  }
+
+  // Given a user and their address, finds the historical transactions on their address.
+  std::string DetectTransactions(const std::string& user) {
+    DetectTransactionsRequest request;
+    request.set_username(user);
+
+    DetectTransactionsResponse response;
+    ClientContext context;
+
+    Status status = stub_->DetectTransactions(&context, request, &response);
+
+    // Check status.
+    if (status.ok()) {
+      if (response.transaction_size() == 0) {
+	 return "Found no transactions for the user";
+      }
+      for (const auto& transaction : response.transaction()) {
+	 std::cout << "From: " << transaction.from_id() <<
+		 " to: " << transaction.to_id() << std::endl;
+      }
+      return "Successfully found user transactions.";
+    } else {
+      std::cout << status.error_code() << ": " << status.error_message() << std::endl;
+      return "RPC failed.";
     }
   }
 
@@ -72,7 +100,7 @@ string findOrReturnDefault(int argc, char** argv, string defaultvalue, string ma
 }
 
 string getAddress(int argc, char** argv) {
-  return findOrReturnDefault(argc, argv, "somebitcoinaddress", "--address");
+  return findOrReturnDefault(argc, argv, "someaddress", "--address");
 }
 
 string getUsername(int argc, char** argv) {
@@ -84,6 +112,15 @@ string getTarget(int argc, char** argv) {
   return findOrReturnDefault(argc, argv, "localhost:50051", "--target");
 }
 
+bool getTransactions(int argc, char** argv) {
+  for (int i = 1; i < argc; i++) {
+      std::string arg_val = argv[i];
+      size_t start_pos = arg_val.find("--getTransactions");
+      if (start_pos != string::npos) { return true; }
+  }
+  return false;
+}
+
 int main(int argc, char** argv) {
   // Instantiate the client. It requires a channel, out of which the actual RPCs
   // are created. This channel models a connection to an endpoint specified by
@@ -93,6 +130,7 @@ int main(int argc, char** argv) {
   std::string target_str = getTarget(argc, argv);
   std::string address = getAddress(argc, argv);
   std::string username = getUsername(argc, argv);
+  bool get_transactions = getTransactions(argc, argv);
   if (address == "") {
         std::cout << "The only correct argument syntax is --address="
                   << std::endl;
@@ -103,7 +141,12 @@ int main(int argc, char** argv) {
   }
   BitcoinTrackerClient bitcoinTracker(
       grpc::CreateChannel(target_str, grpc::InsecureChannelCredentials()));
-  std::string reply = bitcoinTracker.AddAddress(username, address);
+  string reply;
+  if (!get_transactions) {
+    reply = bitcoinTracker.AddAddress(username, address);
+  } else {
+    reply = bitcoinTracker.DetectTransactions(username);
+  }
   std::cout << "BitcoinTracker received: " << reply << std::endl;
 
   return 0;
--- prototype/bitcointrackerserver.cc
+++ prototype/bitcointrackerserver.cc
@@ -1,8 +1,9 @@
-
+#include <algorithm>
 #include <iostream>
 #include <map>
 #include <memory>
 #include <string>
+#include <tuple>
 
 #include <grpcpp/ext/proto_server_reflection_plugin.h>
 #include <grpcpp/grpcpp.h>
@@ -21,17 +22,93 @@ using grpc::Status;
 using helloworld::BitcoinTracker;
 using helloworld::AddAddressRequest;
 using helloworld::AddAddressResponse;
+using helloworld::DetectTransactionsRequest;
+using helloworld::DetectTransactionsResponse;
+using std::get;
+using std::make_tuple;
 using std::string;
 using std::set;
+using std::tuple;
+using std::vector;
 
-// Add user to username.
+// Transaction id, wallet id, timestamp, direction, amount.
+using transaction = std::tuple<string, string, string, string, double>;
+using transactionpairs = std::pair<string, string>;
+
 std::map<string, set<string>> username_address;
+set<string> addresses;
+vector<transaction> transactions;
 
-// Logic and data behind the server's behavior.
+// Custom sort function to sort transactions by timestamp.
+struct {
+	bool operator()(transaction a, transaction b) {
+		return std::get<2>(a) > std::get<2>(b);
+	}
+} lesserTimestampFirst;
+
+// Takes as input the addresses that we need to detect the transactions for, and returns
+// the transaction id pairs.
+vector<transactionpairs> detectTransactions(set<string> addresses) {
+	vector<transactionpairs> matched_transactions;
+	vector<transaction> outgoing;
+	vector<transaction> incoming;
+	// Separate incoming and outgoing transactions.
+	for (const auto& one_transaction : transactions) {
+		// Negative transactions are ignored.
+		if (std::get<4>(one_transaction) <= 0) {
+			continue;
+		}
+		if (std::get<3>(one_transaction) == "out") {
+			outgoing.push_back(one_transaction);
+		} else {
+			incoming.push_back(one_transaction);
+		}
+	}
+	// Sort transactions by timestamp.
+	sort(outgoing.begin(), outgoing.end(), lesserTimestampFirst);
+	sort(incoming.begin(), incoming.end(), lesserTimestampFirst);
+
+	int oi = 0;
+	int ii = 0;
+	string outgoingTimestamp;
+	string incomingTimestamp;
+	// Find matching timestamps.
+	while (oi < outgoing.size() && ii < incoming.size()) {
+		outgoingTimestamp = std::get<2>(outgoing[oi]);
+		incomingTimestamp = std::get<2>(incoming[ii]);
+		if (outgoingTimestamp < incomingTimestamp) {
+				++oi;
+				continue;
+		} else if (incomingTimestamp < outgoingTimestamp) {
+			++ii;
+			continue;
+		}
+		// If the address is from one user, then return matched transactions.
+		if (addresses.find(get<1>(outgoing[oi])) != addresses.end() ||
+				addresses.find(get<1>(incoming[ii])) != addresses.end()) {
+			matched_transactions.push_back(
+				make_pair(std::get<0>(outgoing[oi]),
+					  std::get<0>(incoming[ii])));
+		}
+		oi++;
+		ii++;
+	}
+
+	return matched_transactions;
+}
+
+// Bitcoin tracker server's behavior. Allows adding addresses and detecting transactions
+// for a user.
 class BitcoinTrackerImpl final : public BitcoinTracker::Service {
   Status AddAddress(ServerContext* context, const AddAddressRequest* request,
                   AddAddressResponse* response) override {
     for (const auto& address : request->address()) {
+	    if (addresses.find(address) != addresses.end()) {
+		std::cout << "Address is already claimed.";
+		// TODO(aakanksha): find the right error code to send.
+		continue;
+	    }
+	    addresses.insert(address);
 	    username_address[request->username()].insert(address);
     }
     for (const auto& address : username_address[request->username()]) {
@@ -39,6 +116,24 @@ class BitcoinTrackerImpl final : public BitcoinTracker::Service {
     } 
     return Status::OK;
   }
+
+  Status DetectTransactions(
+		  ServerContext* context,
+		  const DetectTransactionsRequest* request,
+		  DetectTransactionsResponse* response) override {
+    // No address are being tracked for this user, return early.  
+    if (username_address.find(request->username()) == username_address.end()) {
+       return Status::OK;
+    }
+    auto transactionpairs = detectTransactions(username_address[request->username()]);
+
+    for (const auto& tpairs : transactionpairs) {
+	auto* one_transaction = response->add_transaction();
+	one_transaction->set_from_id(tpairs.first);
+	one_transaction->set_to_id(tpairs.second);
+    }
+    return Status::OK;
+  } 
 };
 
 void RunServer() {
@@ -57,6 +152,12 @@ void RunServer() {
   std::unique_ptr<Server> server(builder.BuildAndStart());
   std::cout << "Server listening on " << server_address << std::endl;
 
+  transactions.push_back(make_tuple("1", "someaddress", "2", "out", 4));
+  transactions.push_back(make_tuple("2", "anotheraddress", "2", "out", 4));
+  transactions.push_back(make_tuple("3", "3", "2", "in", 4));
+  transactions.push_back(make_tuple("4", "4", "2", "in", 4));
+  transactions.push_back(make_tuple("5", "someaddress", "4", "in", 4));
+  transactions.push_back(make_tuple("6", "3", "4", "out", 4));
   // Wait for the server to shutdown. Note that some other thread must be
   // responsible for shutting down the server for this call to ever return.
   server->Wait();
